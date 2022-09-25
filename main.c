6bac0a902daac7ac36022a5efbf44e3660b4be7a
Olex1313 <aa1limonov@gmail.com>
2022-09-25 22:48:18 +0300

    add cd implementation

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 
 char **get_input(char *input);
+int cd(char *path);
 
 int main() {
     char **command;
@@ -17,9 +18,26 @@ int main() {
         input = readline("shell9000> ");
         command = get_input(input);
 
+        if (strcmp(command[0], "cd") == 0) {
+            if (cd(command[1]) < 0) {
+                perror(command[1]);
+            }
+            continue;
+        }
+
         child_pid = fork();
+
+        if (child_pid == -1) {
+            perror("Forking failed");
+            exit(1);
+        }
+
         if (child_pid == 0) {
-            execvp(command[0], command);
+            int res = execvp(command[0], command);
+            if (res == -1) {
+                perror(command[0]);
+                exit(1);
+            }
         } else {
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
@@ -34,6 +52,10 @@ int main() {
 
 char **get_input(char *input) {
     char **command = malloc(8 * sizeof(char *));
+    if (command == NULL) {
+        perror("Malloc failed");
+        exit(1);
+    }
     char *separator = " ";
     char *parsed;
     int index = 0;
@@ -49,4 +71,7 @@ char **get_input(char *input) {
     return command;
 }
 
+int cd(char *path) {
+    return chdir(path);
+}
 
